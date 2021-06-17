d9e3aef87b0f2d32eeda75ed47e24539000a372c
HEER-GOHIL <69032140+HEER-GOHIL@users.noreply.github.com>
2021-06-18 01:40:52 +0530

    Update classes_basic.cpp

--- classes_basic.cpp
+++ classes_basic.cpp
@@ -10,14 +10,23 @@ class data{
       cout<<"Color: "<<color<<endl;
       cout<<"Quantity: "<<quant<<endl;
   }
-  
+  //Default Constructor
+  data()
+  {
+      cout<<"default one: will be called everytime an object is declared"<<endl;
+  }
+  //Parameterized Constructor
+  data(string name)
+  {
+      cout<<"Hello Welcome : "<<name<<endl;
+  }
 };
 int main()
 {
-    data car1;
+    data car1; //default contructor will be called
     car1.color = "red";
     car1.quant = 5;
-    data car2;
+    data car2("Anonymous");
     car2.color = "blue";
     car2.quant = 45;
     car1.printdata();
