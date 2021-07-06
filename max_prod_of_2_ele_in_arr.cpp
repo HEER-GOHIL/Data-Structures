d8046dc6ca8486ab851540d2c32cf03170da60ea
HEER-GOHIL <69032140+HEER-GOHIL@users.noreply.github.com>
2021-07-06 19:22:14 +0530

    Create max_prod_of_2_ele_in_arr.cpp

---
+++ max_prod_of_2_ele_in_arr.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        //using the minimum heap (pq) to get the largest two values
+        priority_queue<int, vector<int>, greater<int>>pq;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(pq.size()>=2 && pq.top()<nums[i])
+            {
+                pq.pop();
+                pq.push(nums[i]);
+            }
+            else if(pq.size()<2)
+            {
+                pq.push(nums[i]);
+            }
+        }
+        int ans = 1;
+        while(!pq.empty())
+        {
+            ans = ans*(pq.top()-1);
+            pq.pop();
+        }
+        return ans;
+    }
+};
