a37d5c77d023b5c7f3d738e744b65b194c8afd3c
HEER-GOHIL <69032140+HEER-GOHIL@users.noreply.github.com>
2021-07-07 08:52:44 +0530

    Create Sort_characters_by_frequency.cpp

---
+++ Sort_characters_by_frequency.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        priority_queue<pair<int,char>>pq;
+        unordered_map<char,int>mp;
+        for(int i=0;i<s.length();i++)
+        {
+            mp[s[i]]++;
+        }
+        for(auto [a,b] : mp)
+        {
+            pq.push({b,a});
+        }
+        s = "";
+        while(!pq.empty())
+        {
+            s = s + string(pq.top().first , pq.top().second);
+            pq.pop();
+        }
+        return s;
+    }
+};
