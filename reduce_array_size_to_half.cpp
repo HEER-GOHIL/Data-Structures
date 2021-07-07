d937f22f3d1807e24193c130482be26343c49b6d
HEER-GOHIL <69032140+HEER-GOHIL@users.noreply.github.com>
2021-07-07 06:04:20 +0530

    Create reduce_array_size_to_half.cpp

---
+++ reduce_array_size_to_half.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        int asize = arr.size(); 
+        unordered_map<int,int>mp; //unordered map
+        for(int i=0;i<arr.size();i++)
+        {
+            mp[arr[i]]++ ;//counting the freq!
+        }
+        priority_queue<pair<int,int>>p;
+        for(auto[k, i] : mp)
+        {
+            p.push({i,k});
+        }
+        int flag = 0;
+        int ch = 0;
+        int count = 0;
+        while(flag!=1)
+        {
+            ch = ch + p.top().first;
+            count ++;
+            if(ch >=asize/2)
+            {
+                return count;
+            }
+            p.pop();
+        }
+        return 0;
+    }
+};
