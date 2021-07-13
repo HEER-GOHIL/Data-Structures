3e8236e36b66918b3dbccaa1d34e5eb95a8807a8
HEER-GOHIL <69032140+HEER-GOHIL@users.noreply.github.com>
2021-07-13 12:04:57 +0530

    Create pangram_check.cpp

---
+++ pangram_check.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        // check pangram
+        //every letter appears atleast once
+        map<char,int>mp; //will store the count of the alphabets and it's occurence val
+        for(auto x:sentence)
+        {
+            mp[x]++;
+        }
+        int j=0;
+        for(auto i:mp)
+        {
+            j++;
+            if(i.first == 0)
+            {
+                return false;
+            }
+            else
+            {
+                continue;
+            }
+            j++;
+        }
+        cout<<j;
+        if(j==26)
+        {
+        return true;
+        }
+        return false;
+    }
+};
