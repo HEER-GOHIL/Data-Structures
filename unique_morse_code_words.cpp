6264ac1b8c1d96b2cf18381a552f432f8e25e593
HEER-GOHIL <69032140+HEER-GOHIL@users.noreply.github.com>
2021-07-13 15:53:16 +0530

    Create unique_morse_code_words.cpp

---
+++ unique_morse_code_words.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int uniqueMorseRepresentations(vector<string>& words) {
+        vector<string>decode{".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        map<int,string>mp;
+        map<string,int>out;
+        for(int i=1;i<=decode.size();i++)
+        {
+            mp[i] = decode[i-1];
+        }
+        for(int i=0;i<words.size();i++)
+        {
+            string curr = words[i];
+            string res = "";
+            for(auto j:curr)
+            {
+                res = res + mp[int(j)-96];
+            }
+            out[res]++;
+        }
+        int ret = 0;
+        for(auto x:out)
+        {
+            ret++;
+        }
+        return ret;
+    }
+};
