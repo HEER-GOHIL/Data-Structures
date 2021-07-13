d23312173f12707ab10a677df59f92036391862b
HEER-GOHIL <69032140+HEER-GOHIL@users.noreply.github.com>
2021-07-13 09:02:42 +0530

    Create number_of_good_pairs.cpp

---
+++ number_of_good_pairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        map<int,int>mp;
+        for(int i=0;i<nums.size();i++)
+        {
+            mp[nums[i]]++;
+        }
+        int gpair=0;
+        for(auto i:mp)
+        {
+            int n = i.second;
+            gpair = gpair + n*(n-1)/2;
+        }
+        return gpair;
+    }
+};
