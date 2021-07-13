033207a8cc680984fe0fda7d0b8cdc191b548875
HEER-GOHIL <69032140+HEER-GOHIL@users.noreply.github.com>
2021-07-13 13:02:53 +0530

    Create count_the_number_of_consistent_strings.cpp

---
+++ count_the_number_of_consistent_strings.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int countConsistentStrings(string allowed, vector<string>& words) {
+        map<int,int>mp;
+        for(int i=0;i<allowed.length();i++)
+        {
+            mp[allowed[i]]++;
+        }
+        int retval = 0;
+        for(int i=0;i<words.size();i++)
+        {
+            string curr = words[i];
+            int v = 0; //checking how many number are same 
+            for(auto j:curr)
+            {
+                if(mp[j]>0)
+                {
+                    v++;
+                }
+            }
+            if(v==curr.length())
+            {
+                retval ++;
+            }
+        }
+        return retval;
+    }
+};
