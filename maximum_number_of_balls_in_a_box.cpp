05ea7dec0bc49d68093ade86c11c4798414214b6
HEER-GOHIL <69032140+HEER-GOHIL@users.noreply.github.com>
2021-07-14 07:59:53 +0530

    Create maximum_number_of_balls_in_a_box.cpp

---
+++ maximum_number_of_balls_in_a_box.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countBalls(int low, int high) {
+        unordered_map<int,int>mp;
+        for(int i=low;i<=high;i++)
+        {
+            int dupi = i;
+            int sum = 0;
+            while(dupi!=0)
+            {
+                sum = sum + dupi%10;
+                dupi = dupi/10;
+            }
+            mp[sum]++;
+        }
+        int max = 0;
+        for(auto x:mp)
+        {
+            if(x.second>max)
+            {
+                max = x.second; 
+            }
+        }
+        return max;
+    }
+};
