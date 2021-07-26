405b3059a3deb4e18e5c475e2291c3d2116e1a2d
HEER-GOHIL <69032140+HEER-GOHIL@users.noreply.github.com>
2021-07-26 13:52:28 +0530

    Create robot_return_to_origin_leetcode.cpp

---
+++ robot_return_to_origin_leetcode.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool judgeCircle(string s) {
+        int arr[2];
+        arr[1] = 0;
+        arr[0] = 0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]=='U')
+            {
+                arr[0]++;
+            }
+            if(s[i]=='D')
+            {
+                arr[0]--;
+            }
+            if(s[i]=='R')
+            {
+                arr[1]++;
+            }
+            if(s[i]=='L')
+            {
+                arr[1]--;
+            }
+        }
+        if(arr[0]!=0 || arr[1]!=0)
+        {
+            return false;
+        }
+        return true;
+    }
+};
