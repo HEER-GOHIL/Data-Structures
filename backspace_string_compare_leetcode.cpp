2a483584f5bc70d46e003f69c2b398620f62a5d6
HEER-GOHIL <69032140+HEER-GOHIL@users.noreply.github.com>
2021-07-26 11:58:53 +0530

    Create backspace_string_compare_leetcode.cpp

---
+++ backspace_string_compare_leetcode.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        vector<char>v1;
+        vector<char>v2;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]!='#')
+            {
+                v1.push_back(s[i]);
+            }
+            else
+            {
+                if(v1.size()!=0)
+                {
+                v1.pop_back();
+                }
+            }
+        }
+        for(int i=0;i<t.length();i++)
+        {
+            if(t[i]!='#')
+            {
+                v2.push_back(t[i]);
+            }
+            else
+            {
+                if(v2.size()!=0)
+                {
+                v2.pop_back();
+                }
+            }
+        }
+        if(v1==v2)
+        {
+            return true;
+        }
+        return false;
+    }
+};
