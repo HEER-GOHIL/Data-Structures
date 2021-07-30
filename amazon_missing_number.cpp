c2c9f8f7c5c502da829bd7be7169c24a79aff3d0
HEER-GOHIL <69032140+HEER-GOHIL@users.noreply.github.com>
2021-07-30 11:44:36 +0530

    Update amazon_missing_number.cpp

--- amazon_missing_number.cpp
+++ amazon_missing_number.cpp
@@ -16,3 +16,14 @@ public:
         return 1;
     }
 };
+
+//---------------------second approach-------------------------
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int x = accumulate(nums.begin(), nums.end(), 0);
+        int sum = n*(n+1)/2;
+        return sum - x;
+    }
+};
