ab86c7b9b24bb85995548d28ea67ce2047b742bf
HEER-GOHIL <69032140+HEER-GOHIL@users.noreply.github.com>
2021-07-31 19:16:47 +0530

    Create amazon_k_closest_points.cpp

---
+++ amazon_k_closest_points.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k)     {
+        int dist = 0;
+        priority_queue<pair<int, pair<int, int>>>  pq;
+        // distance and pair of points
+        for(int i=0;i<points.size();i++)
+        {
+            dist = (points[i][0]*points[i][0]) + (points[i][1]*points[i][1]);
+            pq.push({dist,{points[i][0],points[i][1]}});
+            if(pq.size()>k)
+            {
+                pq.pop();
+            }
+        }
+        int i = 0;
+        vector<vector<int>>ans;
+        while(!pq.empty())
+        {
+             vector<int> r={pq.top().second.first, pq.top().second.second};
+            ans.push_back(r);
+            pq.pop();
+        }
+        return ans;
+        
+    }
+};
