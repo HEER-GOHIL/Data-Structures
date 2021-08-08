fddb40c18646970e206075ee21d50c4df68a5d91
HEER-GOHIL <69032140+HEER-GOHIL@users.noreply.github.com>
2021-08-08 21:20:20 +0530

    Create amazon_reverse_vowels.cpp

---
+++ amazon_reverse_vowels.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string reverseVowels(string s) {
+        vector<char>v;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='a' || s[i]=='u'|| s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='A' || s[i]=='U')
+            {
+                v.push_back(s[i]);
+            }
+        }
+        int n = v.size();
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='a' || s[i]=='u'|| s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='A' || s[i]=='U')
+            {
+                s[i] = v[n-1];
+                n--;
+            }
+        }
+        return s;
+    }
+};
