de9e92c293dd768e12e85ccafc980512db1a0c07
HEER-GOHIL <69032140+HEER-GOHIL@users.noreply.github.com>
2021-08-19 23:07:47 +0530

    Create leetcode_hand_of_straights.cpp

---
+++ leetcode_hand_of_straights.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        sort(hand.begin(),hand.end());
+        map<int,int>mp;
+        for(auto x:hand)
+        {
+            mp[x]++;
+        }
+        int n=hand.size();
+        for(int i=0;i<n;i++)
+        {
+            int num = hand[i];
+            if(mp[num]==0)
+            {
+                continue;
+            }
+            for(int z=num;z<num+groupSize;z++)
+            {
+                mp[z]--;
+                if(mp[z]<0)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
